a9b461593fd749513faa736a3ef6555516fb9ee1
victorbubula <victorgiovaninib@gmail.com>
2024-05-24 13:30:55 -0300

    login page

---
+++ src/login.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function Login() {
+
+	return (
+		<>
+			
+			<div className="card">
+				<h1 className="logo">spotycat</h1>
+				<input type="email" name="conta" id="email" />
+				<input type="password" name="senha" id="password" />
+				<input className="botao" type="button" value="login" />
+				
+			</div>
+		</>
+	)
+}
+
+export default Login
