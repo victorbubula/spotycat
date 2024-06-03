05796f528884fc678148d959a91fbb0498851a67
victorbubula <victorgiovaninib@gmail.com>
2024-06-03 15:09:42 -0300

    catman

--- src/login.tsx
+++ src/login.tsx
@@ -5,12 +5,13 @@ function Login() {
 
 	return (
 		<>
-			
-			<div className="card">
-				<h1 className="logo">spotycat</h1>
-				<input type="email" name="conta" id="email" />
-				<input type="password" name="senha" id="password" />
-				<input className="botao" type="button" value="login" />
+			<div className='container'>
+				<div className="card">
+					<h1 className="logo">spotycat</h1>
+					<input type="email" name="conta" id="email" />
+					<input type="password" name="senha" id="password" />
+					<input className="botao" type="button" value="login" />
+				</div>
 				
 			</div>
 		</>
