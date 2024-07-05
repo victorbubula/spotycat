e3ed0856c15d6110ba4ba613e5aa2a0bd0f1a508
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-07-05 05:46:00 -0300

    barra de pesquisa da biblioteca

---
+++ src/components/Sidebar/PesquisaBiblioteca/index.tsx
@@ -0,0 +1,28 @@
+import lupa from '../../../assets/lupa.svg'
+import classNames from "classnames"
+import { useRef, useState } from "react";
+import styles from './PesquisaBiblioteca.module.scss'
+
+const PesquisaBiblioteca = () => {
+    const [barraPesquisa, setBarraPesquisa] = useState(false)
+    const inputRef = useRef<HTMLInputElement>(null);
+    const foco = async () => {
+        await setBarraPesquisa(true)
+        if (inputRef.current)
+            inputRef.current.focus()
+    }
+    return (
+        <div className={classNames({
+            [styles.pesquisa]: true,
+            [styles.barra]: (barraPesquisa) ? true : false
+        })}>
+            <button className={styles.botao} onClick={() => { (barraPesquisa) ? console.log("pesquisar") : foco() }}>
+                <img src={lupa} alt="pesquisar" />
+            </button>
+            <input ref={inputRef} onBlur={()=> setBarraPesquisa(false)} className={styles.buscar} type="search" placeholder='Pesquisar...' />
+            
+        </div>
+    )
+}
+
+export default PesquisaBiblioteca
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,33 +1,50 @@
 import ICard from "../../interfaces/ICard";
 import styles from "./Sidebar.module.scss";
 import img from '../../assets/gato.jpg';
+import PesquisaBiblioteca from "./PesquisaBiblioteca";
 interface props {
     playlists: Array<ICard>
     albums: Array<ICard>
 }
 
 const Sidebar = ({ playlists, albums }: props) => {
-    
     return (
         <div className={styles.container}>
             <div className={styles.inicio}>
                 <p>Inicio</p>
             </div>
             <div className={styles.biblioteca}>
-                <p>Sua Biblioteca</p>
-                <div className={styles.container_lista}>
-                    <div>
-                        <input type="search" />
+                <h1 className={styles.titulo}>Sua Biblioteca</h1>
+                <div className={styles.biblioteca_lista}>
+                    <div className={styles.lista_filtros}>
+                        <PesquisaBiblioteca/>
                         <p>filtro</p>
                     </div>
-                    <ul className={styles.lista}>
-                        {playlists.map((item: ICard) => { return (<li key={item.id} className={styles.card}> <img src={(item.foto==null)?img: item.foto[0].url} alt="" /> <div className={styles.card_descricao}><h1>{item.nome}</h1> <h2>{item.tipo} . {item.artista}</h2></div> </li>) })}
-                        {albums.map((item: ICard) => { return (<li key={item.id} className={styles.card}> <img src={(item.foto==null)?img: item.foto[2].url} alt="" /> <div className={styles.card_descricao}><h1>{item.nome}</h1> <h2>{item.tipo} . {item.artista}</h2></div></li>) })}
+                    <ul className={styles.lista_container}>
+                        {playlists.map((item: ICard) => {
+                            return (
+                                <li key={item.id} className={styles.lista_card}>
+                                    <img src={(item.foto == null) ? img : item.foto[0].url} alt="" />
+                                    <div className={styles.card_descricao}>
+                                        <h1>{item.nome}</h1>
+                                        <h2>{item.tipo} . {item.artista}</h2>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                        {albums.map((item: ICard) => {
+                            return (
+                                <li key={item.id} className={styles.lista_card}>
+                                    <img src={(item.foto == null) ? img : item.foto[2].url} alt="" />
+                                    <div className={styles.card_descricao}>
+                                        <h1>{item.nome}</h1>
+                                        <h2>{item.tipo} . {item.artista}</h2>
+                                    </div>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
-                
-                
-
             </div>
         </div>
     )
